5b2441029781452f12828a7c9dff05d346ea09d8
joaqu1m <joaquim.pires@sptech.school>
2022-11-10 22:25:26 -0300

    mapa com imagens

--- a.js
+++
@@ -1,5 +0,0 @@
-export async function analyticsLogEvents(event, params) {
-    const firebase = await import('firebase/app')
-    await import('firebase/analytics')
-    firebase.default.analytics().logEvent(event, params)
-}
